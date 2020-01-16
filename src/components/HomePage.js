a13d11845c71739981ec8f9ca98044c93de0c6af
tusharojha <tusharojha2001@gmail.com>
2020-01-16 15:06:42 +0530

    Horizontal Carousel Added, Username shown

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import {
-  View,
   Dimensions,
-  Text,
-  ImageBackground,
   StatusBar,
-  StyleSheet
 } from 'react-native';
 import { connect } from 'react-redux';
 import Carousel from 'react-native-snap-carousel';
@@ -13,8 +9,12 @@ import {
   homePageGetInitialFeedData,
   homePageUpdateActiveTab
 } from '../actions';
+import HomePagePost from './homeScreen/HomePagePost';
+
 // Store height(Window + StatusBar) in variable
-const height = Dimensions.get('window').height + StatusBar.currentHeight;
+const WINDOW_HEIGHT = Dimensions.get('window').height + StatusBar.currentHeight;
+// Store height(Window + StatusBar) in variable
+const WINDOW_WIDTH = Dimensions.get('window').width;
 
 class HomePage extends Component {
   // constructor(props) {
@@ -26,45 +26,24 @@ class HomePage extends Component {
     // Getting the Initial Feed Data, when App in opened
     this.props.homePageGetInitialFeedData({ userToken: '' });
   }
-
   // componentWillUnmount() {
   //
   // }
-  // Renders main slider(vertical) with user details
-  MainScreen(item) {
-    return (
-      <ImageBackground source={{ uri: item.uploadUrl }} resizeMode="contain" style={styles.body}>
-        <StatusBar translucent backgroundColor="transparent" />
-        <View style={styles.followTabs}>
-          <Text onPress={() => this.props.homePageUpdateActiveTab({ activeTab: 1 })} style={this.props.activeTab === 1 ? { ...styles.followTabsItem, ...styles.selectedTab } : styles.followTabsItem}>
-            For you
-          </Text>
-          <Text onPress={() => this.props.homePageUpdateActiveTab({ activeTab: 2 })} style={this.props.activeTab === 2 ? { ...styles.followTabsItem, ...styles.selectedTab } : styles.followTabsItem}>Following</Text>
-        </View>
-        <View style={styles.postContent}>  
-            <View style={styles.userDetails}>
-              <Text style={styles.userName}>{item.userId}</Text>
-              <Text style={styles.postCaption}>{item.caption}</Text>
-            </View>
-        </View>
-        <View style={styles.postActionsOptions}>
-          <Text style={{ color: 'red' }}>Hi</Text>
-        </View>
-      </ImageBackground>
-    );
-  }
 
   render() {
     const { feedData, feedPageNum } = this.props;
     console.log('HomePage Feed Data', feedData, feedPageNum);
     return (
-      <Carousel
-        data={feedData}
-        sliderHeight={height}
-        itemHeight={height}
-        vertical
-        renderItem={({ item }) => this.MainScreen(item)}
-      />
+    <Carousel
+      data={feedData}
+      ref={'verticalCarousel'}
+      sliderHeight={WINDOW_HEIGHT}
+      itemHeight={WINDOW_HEIGHT}
+      vertical
+      renderItem={({ item }) => (
+          <HomePagePost WINDOW_WIDTH={WINDOW_WIDTH} feedData item={item} />
+      )}
+    />   
     );
   }
 }
@@ -73,57 +52,6 @@ const mapStateToProps = ({ homePageState }) => {
   const { feedData, feedPageNum, activeTab } = homePageState;
   return { feedData, feedPageNum, activeTab };
 };
-
-const styles = StyleSheet.create({
-  body: {
-      flex: 1,
-      paddingTop: 20,
-  },
-  postActionsOptions: {
-  },
-  followTabs: { 
-      flexDirection: 'row',
-      justifyContent: 'center',
-  },
-  postContent: {
-    position: 'absolute',
-    bottom: 0
-  },
-  userDetails: { 
-      flexDirection: 'column',
-      marginBottom: 60,
-      color: 'white',
-      textShadowColor: 'black',
-      textShadowRadius: 4,
-      marginLeft: 5
-  },
-  userName: {
-      color: 'white',
-      textShadowColor: 'black',
-      textShadowRadius: 4,
-      fontWeight: 'bold',
-      fontSize: 18
-  },
-  postCaption: {
-      color: 'white',
-      textShadowColor: 'black',
-      textShadowRadius: 4,
-      fontSize: 16,
-      width: '65%'
-  },
-  followTabsItem: {
-      color: 'white',
-      fontSize: 24,
-      padding: 10,
-      textShadowColor: 'black',
-      textShadowRadius: 4,
-      opacity: 0.8
-  },
-  selectedTab: {
-      opacity: 1,
-      fontWeight: 'bold',
-  }
-});
 
 
 export default connect(mapStateToProps, {
