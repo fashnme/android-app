154dd390a9c272b7586269a53db40f966d585d0d
tusharojha <tusharojha2001@gmail.com>
2020-02-13 20:14:52 +0530

    Upload Screen partially completed

--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Dimensions, TouchableNativeFeedback } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Dimensions, TouchableNativeFeedback, Modal, Alert, Image } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
+import ImagePicker from 'react-native-image-picker';
 import {
   uploadPageToggleIsSelected,
   uploadPageUpdateCaption,
@@ -13,9 +14,9 @@ import {
 const screenWidth = Dimensions.get('window').width;
 // const height = Dimensions.get('window').height + StatusBar.currentHeight;
 
-const RecordButton = ({ onPress }) => {
+const RecordButton = ({ onPress, onLongPress }) => {
   return (
-    <TouchableNativeFeedback onPress={onPress}>
+    <TouchableNativeFeedback onLongPress={onLongPress} onPress={onPress}>
       <View style={styles.recordButtonOuterCircle}>
         <View style={styles.recordButtonInnerCircle} />
       </View>
@@ -32,7 +33,92 @@ class UploadPage extends Component {
     super();
     this.state = {
       frontCamera: false,
+      isModalVisible: false,
     };
+  }
+  setModalVisible(visible) {
+    this.setState({ isModalVisible: visible });
+  }
+
+  
+ UploadPickerModal() {
+  return (
+    <Modal
+          animationType="slide"
+          visible={this.state.isModalVisible}
+          transparent
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}
+    >
+      <View
+        style={styles.modal}
+      >
+          <View style={styles.modalContainer}>
+            <View>
+              <View> 
+                <TouchableNativeFeedback
+                  onPress={() => {
+                    this.PickImage('image');
+                  }}
+                >  
+                  <View style={styles.item}>
+                    <Text style={{ fontSize: 18 }}>Upload Photo</Text>
+                  </View>
+                </TouchableNativeFeedback>
+                <TouchableNativeFeedback
+                  onPress={() => {
+                    this.PickImage('video');
+                  }}
+                  style={styles.item}
+                >  
+                <View style={styles.item}>
+                    <Text style={{ fontSize: 18 }}>Upload Video</Text>
+                </View>
+                </TouchableNativeFeedback>
+              </View>
+
+              <TouchableNativeFeedback
+                onPress={() => {
+                  this.setModalVisible(!this.state.isModalVisible);
+                }}
+              >
+                <View style={{ alignSelf: 'center' }}>
+                  <Text style={{ color: '#0A79DF', fontSize: 20 }}>Cancel</Text>
+                </View>
+              </TouchableNativeFeedback>
+            </View>
+          </View>
+          </View>
+        </Modal>
+
+  );
+}
+
+  PickImage(mediaType) {
+    const options = {
+      title: 'Select Image/Video',
+      mediaType, 
+      storageOptions: {
+        skipBackup: true,
+        noData: true,
+      },
+    };
+    // Open Image Library:
+    ImagePicker.launchImageLibrary(options, response => {
+      // Same code as in above section!
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+        this.props.selectImageFromLibraryVS(false);
+        // } else if (response.customButton) {
+        //   console.log('User tapped custom button: ', response.customButton);
+      } else {
+        this.props.uploadPageUpdateSelectedImagePath(response.uri);
+        this.props.uploadPageToggleIsSelected(true);
+      }
+    });
   }
 
   render() {
@@ -42,7 +128,16 @@ class UploadPage extends Component {
       if (this.camera) {
         const options = { quality: 0.5, base64: true };
         const data = await this.camera.takePictureAsync(options);
-        console.log(data.uri);
+        this.props.uploadPageUpdateSelectedImagePath(data.uri);
+        this.props.uploadPageToggleIsSelected(true);
+      }
+    };
+    const takeVideo = async () => {
+      if (this.camera) {
+        const options = { quality: 0.5, base64: true };
+        const data = await this.camera.takeVideo(options);
+        this.props.uploadPageUpdateSelectedImagePath(data.uri);
+        this.props.uploadPageToggleIsSelected(true);
       }
     };
     
@@ -79,24 +174,28 @@ class UploadPage extends Component {
         >
           <SafeAreaView style={styles.cameraContainer}>
             <View style={styles.header}>
-              <Icon size={30} color="white" name="cross" type="entypo" iconStyle={styles.icon} />    
+              <TouchableNativeFeedback onPress={() => console.log('Exit Button Pressed')}>
+                <View>
+                  <Icon size={30} color="white" name="cross" type="entypo" iconStyle={styles.icon} />
+                </View>
+              </TouchableNativeFeedback>    
             </View>
             <View style={styles.cameraActions}>
               <CustomIcon name="swap-vertical" type="material-community" onPress={() => this.setState({ frontCamera: !this.state.frontCamera })} />
               <RecordButton onPress={() => takePicture()} />  
-              <CustomIcon name="images" type="entypo" onPress={() => console.log('Gallery Tap')} />
+              <CustomIcon name="images" type="entypo" onPress={() => this.setModalVisible(true)} />
             </View>
           </SafeAreaView>
           </RNCamera>
+          {this.UploadPickerModal()}
         </View>
       );
-    }
-      
-    return (
-      <View>
-        <Text> UploadPage Choose a caption </Text>
-      </View>
-    );
+    } 
+      return (
+        <View style={{ flex: 1, backgroundColor: 'blue' }}>
+          <Image style={{ height: 300, width: 400 }} source={{ uri: selectedImagePath }} />
+        </View>
+      );
   }
 }
 
@@ -109,6 +208,27 @@ const mapStateToProps = ({ homePageState, uploadPageState }) => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
+  },
+  modal: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center' 
+  },
+  modalContainer: { 
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,
+    paddingHorizontal: 60, 
+    paddingVertical: 30, 
+    borderRadius: 10, 
+  },
+  item: {
+    padding: 10,
+    borderRadius: 20,
   },
   cameraContainer: {
     flex: 1,
